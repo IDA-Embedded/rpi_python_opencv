ab225baff549aaa0485f995677b4b1e7ce23b0da
nikolaj <nikolajgliese@tutanota.com>
2022-09-01 15:18:42 +0200

    fixed file not found

--- source/basic_view.py
+++ source/basic_view.py
@@ -13,7 +13,7 @@ class Basic_View:
             try:
                 (res,img) = m_loader.getImage()
                 if(res):
-                    res = ImageAnalysis_Utility.scale(img,0.1)
+                    res = ImageAnalysis_Utility.scale(img,10)
                     ImageAnalysis_Utility.viewSimple(res,"raw")
             except:
                 print("expection happend .... ")
