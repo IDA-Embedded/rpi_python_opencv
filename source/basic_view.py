4f36a2b78e81509716866f1472619f9d617519f3
nikolaj <nikolajgliese@tutanota.com>
2022-09-01 15:00:20 +0200

    fixed file not found

--- source/basic_view.py
+++ source/basic_view.py
@@ -2,6 +2,7 @@ import cv2
 
 from source.ImageAnalysis_Utility import ImageAnalysis_Utility
 from source.Image_Loader import Image_Loader
+
 PATH_TO_IMG = "/home/pi/node_red/images/esp32Img.png"
 class Basic_View:   
 
@@ -9,9 +10,14 @@ class Basic_View:
         m_loader = Image_Loader(PATH_TO_IMG)
 
         while True:
-            (res,img) = m_loader.getImage()
-            if(res):
-                ImageAnalysis_Utility.viewSimple(img,"raw")
+            try:
+                (res,img) = m_loader.getImage()
+                if(res):
+                    ImageAnalysis_Utility.viewSimple(img,"raw")
+            except:
+                print("expection happend .... ")
+                cv2.waitKey(100)
+
            
 
 
